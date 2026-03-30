9699f46567fd102fd9de0f746ef0dedbcca78eea
mimging <6636jj@naver.com>
2026-03-30 19:10:16 +0900

    파일정리

---
+++ 박서준/week02/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("*****아기사자 출석 명단입니다****");
+        Scanner scanner = new Scanner(System.in);
+        //총원 구하기
+        System.out.println("백엔드 세션 인원을 입력해주세요 : ");
+        int people = scanner.nextInt();
+
+        //출석자 수 구하기
+        System.out.println("출석자를 입력해주세요");
+        int attendant = scanner.nextInt();
+
+
+        //결석자 수 구하기
+        System.out.println("결석자 수를 입력해주세요");
+        int absentee = scanner.nextInt();
+
+
+        if (attendant > absentee) {
+            for (int i = 1; i <= attendant; i++) {
+                System.out.println("현재 출석자 수: " + i);
+            }
+            System.out.println("출석자 수 가 결석자 수 보다 많습니다.");
+
+        }
+        else if (attendant == absentee) {
+            System.out.println("석자와 결석자 수가 같네요...");
+        }
+
+        else {
+            System.out.println("다음부터 더 재밌게 운영하세요.");
+        }
+        //데이터 낭비 방지를 위해 입력문 닫기
+        scanner.close();
+    }
+}
